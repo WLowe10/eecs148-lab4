a8f502e6b3ff695fef9b0feb1d0628e42242f9bc
Wes Lowe <wesley.jlowe10@gmail.com>
2025-02-25 15:43:23 -0600

    added practice2 js file

---
+++ practice2.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("style_form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    // const backgroundColorR = formData.get("background_color_r")
+    // const backgroundColorG = formData.get("background_color_g")
+    // const backgroundColorB = formData.get("background_color_b")
+
+    // const borderColorR = formData.get("border_color_r")
+    // const backgroundColorG = formData.get("background_color_g")
+    // const backgroundColorB = formData.get("background_color_b")
+
+    // console.log(formData);
+})
