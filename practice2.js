f195465c1b8bb578967df251f1bc0325ae0fabdf
Wes Lowe <wesley.lowe@yahoo.com>
2025-02-27 14:01:03 -0600

    updated practice 2 to customize box

--- practice2.js
+++ practice2.js
@@ -1,17 +1,23 @@
+const box = document.getElementById("box");
 const form = document.getElementById("style_form");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(form);
+  const formData = new FormData(form);
 
-    // const backgroundColorR = formData.get("background_color_r")
-    // const backgroundColorG = formData.get("background_color_g")
-    // const backgroundColorB = formData.get("background_color_b")
+  const backgroundColorR = Number(formData.get("background_color_r")) || 0;
+  const backgroundColorG = Number(formData.get("background_color_g")) || 0;
+  const backgroundColorB = Number(formData.get("background_color_b")) || 0;
 
-    // const borderColorR = formData.get("border_color_r")
-    // const backgroundColorG = formData.get("background_color_g")
-    // const backgroundColorB = formData.get("background_color_b")
+  const borderColorR = Number(formData.get("border_color_r")) || 0;
+  const borderColorG = Number(formData.get("border_color_g")) || 0;
+  const borderColorB = Number(formData.get("border_color_b")) || 0;
 
-    // console.log(formData);
-})
+  const borderWidth = Number(formData.get("border_width")) || 0;
+
+  box.style.borderWidth = borderWidth;
+  box.style.borderStyle = "solid";
+  box.style.backgroundColor = `rgb(${backgroundColorR}, ${backgroundColorG}, ${backgroundColorB})`;
+  box.style.borderColor = `rgb(${borderColorR}, ${borderColorG}, ${borderColorB})`;
+});
